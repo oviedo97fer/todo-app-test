56b29b13ac3ed65b280dbe5777662369f00c0fe2
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2025-06-16 17:13:38 -0300

    todo store

--- store/useToDoStore.tsx
+++ store/useToDoStore.tsx
@@ -5,6 +5,7 @@ import { createJSONStorage, persist } from "zustand/middleware";
 export interface ToDoState {
     todos: ToDoNote[];
     setTodos: (todos: ToDoNote[]) => void;
+    deleteTodo: (id: string) => void;
     newTodo: string;
     setNewTodo: (newTodo: string) => void;
 }
@@ -14,6 +15,7 @@ export const useToDoStore = create<ToDoState>()(
         (set, get) => ({
             todos: [],
             newTodo: "",
+            deleteTodo: (id: string) => set({ todos: get().todos.filter((todo) => todo.id !== id) }),
             setTodos: (todos) => set({ todos }),
             setNewTodo: (newTodo) => set({ newTodo })
         }),
